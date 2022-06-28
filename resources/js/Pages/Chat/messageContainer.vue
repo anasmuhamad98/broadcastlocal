d8fded1ee81de944d80e3d9c67dede7f03b5b223
unknown <anasmuhamad98@gmail.com>
2022-06-28 10:52:22 +0800

    first

---
+++ resources/js/Pages/Chat/messageContainer.vue
@@ -0,0 +1,62 @@
+<script setup>
+import messageItem from "./messageItem.vue";
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>TIME</th>
+        <th>TO</th>
+        <th>FROM</th>
+        <th>TEXT</th>
+        <th>ACTION</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(message, index) in messages" :key="index">
+        <message-item :message="message" />
+      </tr>
+    </tbody>
+  </table>
+  <!-- <div class="h-96 w-full">
+    <div class="h-full p-2 flex flex-col-reverse overflow-scroll">
+      <div v-for="(message, index) in messages" :key="index">
+        <message-item :message="message" />
+      </div>
+    </div>
+  </div> -->
+</template>
+
+<script>
+export default {
+  components: { messageItem },
+  props: ["messages"],
+  mounted() {
+    this.scrollToElement();
+  },
+  mounted() {
+    scrollToBottom();
+  },
+  methods: {
+    scrollToBottom() {
+      const container = this.$refs.container;
+      container.scrollTop = container.scrollHeight;
+    },
+  },
+};
+</script>
+
+<style>
+tbody {
+  display: block;
+  height: 500px;
+  overflow: auto;
+}
+thead,
+tbody tr {
+  display: table;
+  width: 100%;
+  table-layout: fixed;
+}
+</style>
---
+++ resources/js/Pages/Chat/messageItem.vue
@@ -0,0 +1,13 @@
+<template>
+        <td class="text-center">{{message.created_at}}</td>
+        <td class="text-center"></td>
+        <td class="text-center">{{message.user.name}}</td>
+        <td class="text-center">{{message.message}}</td>
+        <td class="text-center"></td>
+</template>
+
+<script>
+export default{
+    props: ['message']
+}
+</script>
